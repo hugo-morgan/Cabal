7cc8c9f86768b1815601880be01cc472f86c6921
Morgan <hugo.morgam.unb@gmail.com>
2024-02-10 19:04:31 -0300

    Adicionado funcao BM2 para o ultimo boss <- DU

--- arena_7.py
+++ arena_7.py
@@ -7,43 +7,62 @@ import skills as sk
 import keyboard as kb
 
 
-def killBoss():
-    # if bm == 'bm2':
-    #     sk.ligar_bm2()
-    #     start = time.time()
-    #     hp = 1
-    #     while hp == 1:
-    #         end = time.time()
-    #
-    #         try:
-    #             x, y = localiza('img\\hpBoss.png', 0.9)
-    #             sk.bm2_atack()
-    #             sk.defesa()
-    #             sk.potar()
-    #         except ImageNotFoundException:
-    #             print(f"Mob morto.")
-    #             hp = 0
-    # else:
-    #     hp = 1
-    #     while hp == 1:
-    #         try:
-    #             x, y = localiza('img\\hpBoss.png', 0.9)
-    #             sk.dano()
-    #             sk.defesa()
-    #             sk.potar()
-    #         except ImageNotFoundException:
-    #             print(f"Mob morto.")
-    #             hp = 0
-    hp = 1
-    while hp == 1:
-        try:
-            x, y = localiza('img\\hpBoss.png', 0.9)
-            sk.dano()
-            sk.defesa()
-            sk.potar()
-        except ImageNotFoundException:
-            print(f"Mob morto.")
-            hp = 0
+def killBoss(bm=""):
+    if bm == 'bm2': # 90s de BM2
+        sk.ligar_bm2()
+        start = time.time()
+        hp = 1
+        bm2 = True
+        print("BM2 ativa")
+        while hp == 1:
+            end = time.time()
+            tempo_bm = end - start
+
+            if tempo_bm <= 10.00:
+                try:
+                    x, y = localiza('img\\hpBoss.png', 0.9)
+                    sk.bm2_atack()
+                    sk.defesa()
+                    sk.potar()
+                except ImageNotFoundException:
+                    print(f"Mob morto.")
+                    hp = 0
+            elif tempo_bm > 10.00 and bm2 == True:
+                clickD(192,116) # Desliga BM
+                bm2 = False
+                print("BM2 finalizada")
+                try:
+                    x, y = localiza('img\\hpBoss.png', 0.9)
+                    sk.dano()
+                    sk.defesa()
+                    sk.potar()
+                except ImageNotFoundException:
+                    print(f"Mob morto.")
+                    hp = 0
+            else:
+                try:
+                    x, y = localiza('img\\hpBoss.png', 0.9)
+                    sk.dano()
+                    sk.defesa()
+                    sk.potar()
+                except ImageNotFoundException:
+                    print(f"Mob morto.")
+                    hp = 0
+
+
+    # FUNCIONAL ABAIXO SEM BM #
+    # def killBoss():
+    # hp = 1
+    # while hp == 1:
+    #     try:
+    #         x, y = localiza('img\\hpBoss.png', 0.9)
+    #         sk.dano()
+    #         sk.defesa()
+    #         sk.potar()
+    #     except ImageNotFoundException:
+    #         print(f"Mob morto.")
+    #         hp = 0
+    ##################
 
 def killGate():
     hp = 1
@@ -75,30 +94,10 @@ def clickE(x, y):
     py.click(x, y, button="left")
     sleep(1)
 
-
-def captar_mouse():
-    script = []
-    quit = False
-    while quit == False:
-        if kb.is_pressed('9'):
-            x, y = py.position()
-            mousepos = [x, y, 'sk.esquiva()']
-            script.append(mousepos)
-            print(script)
-            sleep(1)
-
-        if kb.is_pressed('0'):
-            x, y = py.position()
-            mousepos = [x, y, 'sk.desliza()']
-            script.append(mousepos)
-            print(script)
-            sleep(1)
-
-        if kb.is_pressed('shift'):
-            quit = True
-            sleep(1)
-    return script
-
+def clickD(x, y):
+    py.moveTo(x, y, duration=0.1)
+    py.click(x, y, button="right")
+    time.sleep(1)
 
 def executar_script(script):
     clickE(955, 556)  # click personagem
@@ -111,7 +110,6 @@ def executar_script(script):
         if _[2] == 'sk.esquiva()':
             py.moveTo(x, y)
             sk.esquiva()
-
 
 def play():
     print("entrando arena 7")
@@ -138,7 +136,7 @@ def play():
         try:
             x, y = localiza('img\\hpBoss.png', 0.9)
             sk.over()
-            killBoss()
+            killBoss('bm2')
             mobsVivos = False
         except ImageNotFoundException:
             print("Mobs vivos ainda!")
@@ -164,4 +162,3 @@ def play():
     clickE(321, 554)  # sai da dg
     sleep(2)
     print("dg finalizada")
-play()
